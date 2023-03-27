db4626929d7846b3b669e238c8d97b3d5597b0fb
Darshan Ghorpade <mr.darshanghorpade@gmail.com>
2023-03-27 23:28:54 +0530

    Initial Commit

---
+++ OOPDemo/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    public class Person
+    {
+        //fields
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //ctor: shortcut for constructor
+        // constructor: to do some actions at the time of instantiation of the class
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = LastName;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // new operator: to allocate memory to that object
+            Person person1 = new Person("Darshan", "Ghorpade");
+            person1.Introduce();
+
+            System.Console.ReadLine();
+        }
+    }
+}
