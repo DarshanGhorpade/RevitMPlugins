6037836e70eccad31f7d914e6e0fdfb2bfd8c944
Darshan Ghorpade <mr.darshanghorpade@gmail.com>
2023-04-01 21:07:00 +0530

    Out and ref keywords

--- OOPDemo/Person.cs
+++ OOPDemo/Person.cs
@@ -1,28 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOPDemo
 {
     public class Person
     {
         //fields
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
+        public string FirstName;
+        public string LastName;
         //ctor: shortcut for constructor
         // constructor: to do some actions at the time of instantiation of the class
-        public Person(string firstName, string lastName)
+
+
+        public Person(string firstName)
         {
             this.FirstName = firstName;
-            this.LastName = LastName;
         }
+
+        public Person(string firstName, string lastName)
+            :this(firstName)
+        {
+            this.LastName = lastName;
+        }
+
+        public Person() 
+            :this("DefaultFirstName", "DefaultLastName")
+        { }
 
         public void Introduce()
         {
-            Console.WriteLine(FirstName + " " + LastName);
+            Console.WriteLine(FirstName);
+        }
+
+        public string GetFullName()
+        {
+            var fullName = $"{FirstName}_{LastName}";   // string interpolation
+            return fullName ;
+        }
+
+        public string GetFullName(string separator)
+        {
+            var fullName = $"{FirstName}{separator}{LastName}";   // string interpolation
+            return fullName;
+        }
+
+        public double Calculate(params double[] values)
+        {
+            var sum = values.Sum();
+            return sum;
         }
     }
 }
--- OOPDemo/Program.cs
+++ OOPDemo/Program.cs
@@ -1,14 +1,59 @@
-﻿namespace OOPDemo
+﻿using System;
+
+namespace OOPDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // new operator: to allocate memory to that object
-            Person person1 = new Person("Darshan", "Ghorpade");
-            person1.Introduce();
+            Person person1 = new Person() { FirstName = "Darshan", LastName = "Ghorpade" };
+            // person1.Introduce();
+            //Console.WriteLine(person1.GetFullName("+"));
 
-            System.Console.ReadLine();
+            var testClass = new TestClass();
+            //testClass.DoSomething(person1);
+            //Console.WriteLine(person1.GetFullName("+"));
+
+            /*
+             * ref & out difference
+             * ref requires the varible to be created already
+             * out doesn't requires the variable to be created already
+             */
+
+            var value = 15;
+            //Console.WriteLine(value);
+            //testClass.DoSomething(ref value);
+            //testClass.DoSomething(out var value);
+            //Console.WriteLine(value);
+
+            var text = "10";
+            var CanBeParsed = double.TryParse(text, out var number);
+            if(CanBeParsed)
+                Console.WriteLine(number);
+
+            //var valuesToCalculate = new double[] { 1.5, 2.4, 1.1, 5.0};
+            //Console.WriteLine(person1.Calculate(valuesToCalculate));
+            //Console.WriteLine(person1.Calculate(1, 2, 3, 4));
+
+            Console.ReadLine();
+        }
+    }
+
+    public class TestClass {
+        //public void DoSomething(Person person)
+        //{
+        //    person.FirstName = "NewName";
+        //}
+        //public void DoSomething(ref int value)
+        //{
+        //    value = 10;
+        //}
+        public bool DoSomething(out int value)
+        {
+            value = 0;
+            return true;
+            //value = 10;
         }
     }
 }
