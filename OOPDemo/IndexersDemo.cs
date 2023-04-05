6bccfba29e454b3968fab54f07a09c615ee95312
Darshan Ghorpade <mr.darshanghorpade@gmail.com>
2023-04-05 08:11:37 +0530

    Adding Indexers Demo

---
+++ OOPDemo/IndexersDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOPsDemo
+{
+    internal class IndexersDemo
+    {
+        static void Main(string[] args)
+        {
+            Element element = new Element();
+            Parameter parameter = element[BuiltInParameter.Mark];
+            parameter.value = "Mark";
+        }
+    }
+
+    public class Parameter
+    {
+        public BuiltInParameter BuiltInParameter { get; }
+        public string value { get; set; }
+
+        public Parameter(BuiltInParameter builtInParameter)
+        {
+            BuiltInParameter = builtInParameter;
+        }
+    }
+
+    public class Element
+    {
+        public Parameter this[BuiltInParameter builtInParameter]
+        {
+            get
+            {
+                return new Parameter(builtInParameter);
+            }
+        }
+    }
+}
