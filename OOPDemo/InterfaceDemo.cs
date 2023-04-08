9acd4d141c0c3f84680ab597d5c07e72425874b8
Darshan Ghorpade <mr.darshanghorpade@gmail.com>
2023-04-08 20:15:55 +0530

    Adding interface demo

---
+++ OOPDemo/InterfaceDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPsDemo
+{
+    public class Circle : IShape
+    {
+        public bool IsVisible { get; set; }
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+        }
+    }
+
+    public class Rectangle : IShape
+    {
+        public bool IsVisible { get; set; }
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle");
+        }
+    }
+
+    public class Canvas
+    {
+        public void DrawShapes(List<IShape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+
+    internal class InterfaceDemo
+    {
+        static void Main(string[] args)
+        {
+            var canvas = new Canvas();
+            var shapes = new List<IShape>
+            {
+                new Circle(),
+                new Rectangle()
+            };
+            canvas.DrawShapes(shapes);
+            Console.ReadLine();
+        }
+    }
+}
