c77710381d26cea73defaeaddc66b959620c3988
Darshan Ghorpade <darshan.ghorpade@ncircletech.com>
2023-04-16 17:18:09 +0530

    Adding more extension methods

--- CustomRevitLibraries/CoreRevitLibrary/Extensions/DocumentExtensions.cs
+++ CustomRevitLibraries/CoreRevitLibrary/Extensions/DocumentExtensions.cs
@@ -37,6 +37,129 @@ namespace CoreRevitLibrary.Extensions
 
             return elements;
         }
+
+        /// <summary>
+        /// Returns the element by a given name
+        /// </summary>`
+        /// <typeparam name="TElement">The element type</typeparam>
+        /// <param name="document">Document</param>
+        /// <param name="name">The element name</param>
+        /// <returns>The element</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TElement GetElementByName<TElement>(this Document document, string name)
+            where TElement : Element
+        {
+            var element = new FilteredElementCollector(document)
+                .OfClass(typeof(TElement))
+                .FirstOrDefault(e => e.Name == name);
+            if (element == null)
+                throw new ArgumentNullException($"The element of given name : {name} is not present in the document");
+            return element as TElement;
+        }
+
+        /// <summary>
+        /// Return the elements by types
+        /// </summary>
+        /// <typeparam name="TElement1">The first element type</typeparam>
+        /// <typeparam name="TElement2">The second element type</typeparam>
+        /// <param name="document">Document</param>
+        /// <returns>The elements</returns>
+        public static List<Element> GetElementsByTypes<TElement1, TElement2>(this Document document)
+            where TElement1 : Element
+            where TElement2 : Element
+        {
+            var types = new List<Type>()
+            {
+                typeof(TElement1),
+                typeof(TElement2)
+            };
+            var multiClassFilter = new ElementMulticlassFilter(types);
+            return new FilteredElementCollector(document)
+                .WherePasses(multiClassFilter)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Return the elements by types
+        /// </summary>
+        /// <typeparam name="TElement1">The first element type</typeparam>
+        /// <typeparam name="TElement2">The second element type</typeparam>
+        /// <typeparam name="TElement3">The third element type</typeparam>
+        /// <param name="document">Document</param>
+        /// <returns>The elements</returns>
+        public static List<Element> GetElementsByTypes<TElement1, TElement2, TElement3>(this Document document)
+            where TElement1 : Element
+            where TElement2 : Element
+            where TElement3 : Element
+        {
+            var types = new List<Type>()
+            {
+                typeof(TElement1),
+                typeof(TElement2),
+                typeof(TElement3)
+            };
+            var multiClassFilter = new ElementMulticlassFilter(types);
+            return new FilteredElementCollector(document)
+                .WherePasses(multiClassFilter)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Return the elements by types
+        /// </summary>
+        /// <typeparam name="TElement1">The first element type</typeparam>
+        /// <typeparam name="TElement2">The second element type</typeparam>
+        /// <typeparam name="TElement3">The third element type</typeparam>
+        /// <typeparam name="TElement4">The fourth element type</typeparam>
+        /// <param name="document">Document</param>
+        /// <returns>The elements</returns>
+        public static List<Element> GetElementsByTypes<TElement1, TElement2, TElement3, TElement4>(this Document document)
+            where TElement1 : Element
+            where TElement2 : Element
+            where TElement3 : Element
+            where TElement4 : Element
+        {
+            var types = new List<Type>()
+            {
+                typeof(TElement1),
+                typeof(TElement2),
+                typeof(TElement3),
+                typeof(TElement4)
+            };
+            var multiClassFilter = new ElementMulticlassFilter(types);
+            return new FilteredElementCollector(document)
+                .WherePasses(multiClassFilter)
+                .ToList();
+        }
+
+        /// <summary>
+        /// It returns elements by types
+        /// </summary>
+        /// <param name="document">Document</param>
+        /// <param name="types">Types</param>
+        /// <returns>The elements</returns>
+        public static List<Element> GetElementsByTypes(this Document document, params Type[] types)
+        {
+            if (!types.Any()) throw new ArgumentNullException("There are no types");
+            var multiClassFilter = new ElementMulticlassFilter(types);
+            return new FilteredElementCollector(document)
+                .WherePasses(multiClassFilter)
+                .ToList();
+        }
+
+        /// <summary>
+        /// It returns elements by types
+        /// </summary>
+        /// <param name="document">Document</param>
+        /// <param name="types">Types</param>
+        /// <returns>The elements</returns>
+        public static List<Element> GetElementsByTypes(this Document document, List<Type> types)
+        {
+            var multiClassFilter = new ElementMulticlassFilter(types);
+            return new FilteredElementCollector(document)
+                .WherePasses(multiClassFilter)
+                .ToList();
+        }
     }
 }
 
